57664f717bd1f0d77de81983bc48b8d0d85fbd42
Laptop <trannamhung2001@gmail.com>
2024-02-02 14:24:10 -0600

    Make a UnitTest for Price object

---
+++ app/src/test/java/com/example/easyshopper/objects/PriceTest.java
@@ -0,0 +1,30 @@
+package com.example.easyshopper.objects;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PriceTest {
+
+    @Before
+    public void setup() {System.out.println("Starting test for Price");
+    }
+
+    @Test
+    public void testCreateAPrice() {
+        System.out.println("\nStarting testCreateAPrice");
+
+        //Create Price to use for tests
+        Price price = new Price(13, 13, 2.99);
+
+        //Test GETTER methods
+        assertNotNull(price);
+        assertEquals(13, price.getStoreID());
+        assertEquals(13, price.getProductID());
+        assertEquals(2.99, price.getPrice(), 0.00001);
+
+        System.out.println("Finished testCreateAPrice");
+    }
+}
+
+
