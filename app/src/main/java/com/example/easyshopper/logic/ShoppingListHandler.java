2abb01c87e3dbeabbe03f2f707d4eb949e21421f
ty842 <taoy4@myumanitoba.ca>
2024-02-02 19:53:29 -0600

    code of ShoppingListHandler

--- app/src/main/java/com/example/easyshopper/logic/ShoppingListHandler.java
+++ app/src/main/java/com/example/easyshopper/logic/ShoppingListHandler.java
@@ -1,5 +1,12 @@
 package com.example.easyshopper.logic;
 
+import com.example.easyshopper.application.Services;
+import com.example.easyshopper.objects.Product;
+import com.example.easyshopper.objects.ShoppingList;
+import com.example.easyshopper.objects.Store;
+import com.example.easyshopper.persistence.ShoppingListPersistence;
+
+
 //Handles the shopping lists
 public class ShoppingListHandler {
 
@@ -12,4 +19,60 @@ public class ShoppingListHandler {
     *   Removing an item from a shopping list
     *   Does a list contain a certain item
      */
+
+    //construct
+    public ShoppingListHandler(){
+    }
+
+    public String getShoppingListByName(){
+        return null;
+    }
+
+    //add item into the given shopping list by id
+    public void addItemToList(Product newProduct, int shoppingListId){
+        Services service = new Services();
+        ShoppingListPersistence shoppingListPersistence = service.getShoppingListPersistence();
+
+        ShoppingList list = shoppingListPersistence.getShoppingListById(shoppingListId);
+
+        list.addProductToCart(newProduct);
+    }
+
+    //!!
+    //create a new shopping list and add to overall shopping list array
+    //idk How shoppingListId work, does it will be gaven as parameter with this function?
+    public void createShoppingList(int shoppingListId, Store store){
+        ShoppingList newList = new ShoppingList(shoppingListId, store);
+
+        Services service = new Services();
+        ShoppingListPersistence shoppingListPersistence = service.getShoppingListPersistence();
+
+        shoppingListPersistence.addShoppingList(newList);
+    }
+
+    //remove given shoppingList from overall shopping list array
+    public void removeShoppingList(int shoppingListId){
+        Services service = new Services();
+        ShoppingListPersistence shoppingListPersistence = service.getShoppingListPersistence();
+
+        shoppingListPersistence.deleteShoppingListById(shoppingListId);
+    }
+
+    //remove an item from a shopping list
+    public void removeProduct(Product product, int shoppingListId){
+        Services service = new Services();
+        ShoppingListPersistence shoppingListPersistence = service.getShoppingListPersistence();
+
+        ShoppingList list = shoppingListPersistence.getShoppingListById(shoppingListId);
+        list.removeProductFromCart(product);
+    }
+
+    //Does a list contain an item
+    public boolean checkItemExistInList(Product product, int shoppingListId){
+        Services service = new Services();
+        ShoppingListPersistence shoppingListPersistence = service.getShoppingListPersistence();
+
+        ShoppingList list = shoppingListPersistence.getShoppingListById(shoppingListId);
+        return list.checkForProductInCart(product.getProductName());
+    }
 }
