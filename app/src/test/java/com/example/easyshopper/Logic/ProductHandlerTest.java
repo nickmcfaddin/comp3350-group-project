de82d7bf7cd0be16720e4f370ccc714769618eda
Baiaioo <jarett_k@live.ca>
2024-02-07 13:40:50 -0600

    Product handler test. 2 test s

--- app/src/test/java/com/example/easyshopper/Logic/ProductHandlerTest.java
+++ app/src/test/java/com/example/easyshopper/Logic/ProductHandlerTest.java
@@ -1,15 +1,21 @@
 package com.example.easyshopper.Logic;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import com.example.easyshopper.logic.ProductHandler;
 import com.example.easyshopper.objects.Price;
 import com.example.easyshopper.objects.Product;
+import com.example.easyshopper.persistence.StorePersistence;
+import com.example.easyshopper.persistence.PricePersistence;
+import com.example.easyshopper.persistence.ProductPersistence;
+import com.example.easyshopper.persistence.stub.ProductPersistenceStub;
 
 import org.junit.Before;
 import org.junit.Test;
 
 
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -23,29 +29,35 @@ public class ProductHandlerTest {
     }
 
 
+    @Test
+    public void testGetProductsByName() {
+        System.out.println("\nStarting testing testGetProductsByName");
+        List<Product> testList = productHandler.getProductsByName("Apple");
 
-
-    /*
-     * fake prices and products for testing
-     */
-    Product product1 = new Product(1,"apple",1.1,1.0,1.0);
-    Price appleP1 = new Price(1,1,1.00);
-    Price appleP2 = new Price(2,1,1.25);
-    Price appleP3 = new Price(2,1,0.95);
-
-    Product product2 = new Product(2,"pie",2.1,9.0,2.0);
-    Price pieP1 = new Price(1,2,5.00);
-    Price pieP2 = new Price(2,2,5.25);
-    Price pieP3 = new Price(2,2,4.95);
+        assertNotNull(testList);
+        assertTrue("Apple is in the list ", testList.contains("Apple"));
+        System.out.println("Finished testGetProductsByName");
+    }
 
 
     @Test
     public void testAllStoreSortedPrice(){
         System.out.println("\nStarting testing testAllStoreSortedPrice");
+        ProductPersistence ProductPersistenceStub = new ProductPersistenceStub();
+        Product product = ProductPersistenceStub.getProductById(1);
+        assertTrue((product.getProductName().equals("Apple")));
 
-        List<Price> priceList = productHandler.allStoreSortedPrice(product1);
-
+        List<Price> priceList = productHandler.allStoreSortedPrice(product);
         assertNotNull(priceList);
+
+        Iterator<Price> iter = priceList.iterator();
+        Price current = null, previous = iter.next();
+        while(iter.hasNext()){
+            current = iter.next();
+        }if(previous.getPrice()<current.getPrice()) {
+            assertTrue(false);
+        }else assertTrue(true);
+
         priceList.forEach(System.out::println);
         System.out.println("Finished testAllStoreSortedPrice");
     }
